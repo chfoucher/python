bb5f0561d80e0f27791f273095fb0c32c73d39d8
chfoucher <pele.foucher@free.fr>
2022-07-23 16:21:28 +0200

    Avoids walls

--- labyrinth.py
+++ labyrinth.py
@@ -16,8 +16,41 @@ def logMap(map):
     for row in map:
         log(row)
 
-def searchTarget(target, map):
-    action = "LEFT" if target == "T" else "RIGHT"
+directions = ["UP", "DOWN", "LEFT", "RIGHT"]
+moves = {
+    "UP": [0, -1],
+    "DOWN": [0, 1],
+    "LEFT": [-1, 0],
+    "RIGHT": [1, 0],
+}
+
+def computeCostMap(target, map, kr, kc):
+    costMap = {}
+    #log("{0}{1}{2}".format(map[kr-1][kc-1],map[kr-1][kc],map[kr-1][kc+1]))
+    for dir in directions:
+        log(moves[dir])
+        destination = map[kr + moves[dir][1]][kc + moves[dir][0]]
+        log("dir: {0}, type {1}".format(dir, destination))
+        if destination == target:
+            costMap[dir] = 0
+        elif destination == "?":
+            costMap[dir] = 1
+        elif destination == ".":
+            costMap[dir] = 2
+        else:
+            costMap[dir] = 10
+    return costMap
+
+def searchTarget(target, map, kr, kc):
+    log("kr = {0}, kc = {1}".format(kr, kc))
+    action = directions[0]
+    costMap = computeCostMap(target, map, kr, kc)
+    log(costMap)
+    minCost = costMap[action]
+    for dir in directions[1:]:
+        if minCost > costMap[dir]:
+            minCost = costMap[dir]
+            action = dir
     return action
     
 # r: number of rows.
@@ -38,4 +71,4 @@ while True:
         log("Command center reached!")
 
     # Rick's next move (UP DOWN LEFT or RIGHT).
-    print(searchTarget(target, map))
+    print(searchTarget(target, map, kr, kc))
